697a1c342a2b095216dc744dff4b49077ade9180
Muhammad Fazil Qureshi <softmfq@gmail.com>
2018-05-08 19:53:21 +0500

    Added React-Router-Dom and reactstrap

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+const Header = () => (
+  <header>
+    <Navbar color="dark" dark expand="md">
+      <NavbarBrand href='/'>
+        Bitrex Market
+      </NavbarBrand>
+    </Navbar>
+  </header>
+)
+
+export default Header
