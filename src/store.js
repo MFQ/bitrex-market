b0ad00b07dafdab03a6b82e946bf204c37cdff2a
Muhammad Fazil Qureshi <softmfq@gmail.com>
2018-05-09 17:30:43 +0500

    initialize store

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import reducer from "./reducers";
+
+const middlewares= [thunk, logger]
+
+export default createStore(reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(...middlewares),
+);
