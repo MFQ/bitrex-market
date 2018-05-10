87626e1494d5b755eb62d0b371a3583118065b38
Muhammad Fazil Qureshi <softmfq@gmail.com>
2018-05-10 15:59:20 +0500

    refactor reducers

---
+++ src/actions/currency.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { getUrl } from '../utils/api';
+export function fetchCurrencyDetail(currencyId) {
+  return (dispatch) => {
+    dispatch( { type: 'FETCH_CURRENCY_BEGAIN' } )
+    axios.get( getUrl(`pubticker/${currencyId}`) )
+    .then((res) => dispatch( { type: 'FETCH_CURRENCY_FULLFILLED', payload: { currency: res.data } } ) )
+    .catch( (err) => dispatch ({ type: 'FETCH_CURRENCY_REJECT', payload: { error: err } }) );
+  }
+}
+
+export function fetchCurrencyBook(currencyId) {
+  return (dispatch) => {
+    dispatch( { type: 'FETCH_CURRENCY_BOOK_BEGAIN' } )
+    axios.get( getUrl(`book/${currencyId}`) )
+    .then((res) => dispatch( { type: 'FETCH_CURRENCY_BOOK_FULLFILLED', payload: { book: res.data } } ) )
+    .catch( (err) => dispatch ({ type: 'FETCH_CURRENCY_BOOK_REJECT', payload: { error: err } }) );
+  }
+}
--- src/actions/market.js
+++ src/actions/market.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { getUrl } from '../utils/api';
 
 export function fetchMarkets() {
   return (dispatch) => {
     dispatch( { type: 'FETCH_MARKETS_BEGAIN' } )
-    axios.get("https://cors-anywhere.herokuapp.com/https://api.bitfinex.com/v1/symbols_details")
+    axios.get( getUrl('symbols_details') )
     .then((res) => dispatch( { type: 'FETCH_MARKETS_FULLFILLED', payload: { symbols: res.data } } ) )
     .catch( (err) => dispatch ({ type: 'FETCH_MARKETS_REJECT', payload: { error: err } }) );
   }
